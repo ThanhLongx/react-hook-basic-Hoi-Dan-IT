0228f97ebfb47fb9cf899b878bf0ed80287a78e2
Huynh Thanh Long <Huynhthanhlongcm123@gmail.com>
2023-07-24 17:08:59 +0700

    two commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Nav from "./Views/Nav";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello world with React and Hoi Dan IT!</h1>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Nav />
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1>Hello world with React and Hoi Dan IT!</h1>
+            </header>
+        </div>
+    );
 }
 
 export default App;
